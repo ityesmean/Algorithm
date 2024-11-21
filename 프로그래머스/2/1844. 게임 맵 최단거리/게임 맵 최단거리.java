a3034eac8a84f5badec5dfb759e06e44c128024f
ityesmean <81310403+ityesmean@users.noreply.github.com>
2024-11-21 23:45:53 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.91 ms, Memory: 52.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,57 +1,58 @@
 import java.util.*;
 
 class Solution {
-    static boolean[][] visit;
+    
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-    static int Min = Integer.MAX_VALUE;
+    static int answer;
+    static boolean[][] visit;
     
     public int solution(int[][] maps) {
-        
+        answer = Integer.MAX_VALUE;
         visit = new boolean[maps.length][maps[0].length];
         
-        bfs(maps, 1);
+        bfs(maps, 0, 0);
         
-        if (Min == Integer.MAX_VALUE) {
-            return -1;
-        } else {
-            return Min;
-        }
+        if (answer == Integer.MAX_VALUE)
+            answer = -1;
+        
+        return answer;
     }
     
-    void bfs(int[][] maps, int count) {
+    public static void bfs(int[][] maps, int x, int y) {
         Queue<Node> q = new LinkedList<Node>();
-        q.add(new Node(0, 0, count));
-        visit[0][0] = true;
+        q.add(new Node(x, y, 0));
+        visit[x][y] = true;
         
         while (!q.isEmpty()) {
+            
             Node tmp = q.poll();
             
-            if (tmp.x == maps.length - 1 && tmp.y == maps[0].length - 1) {
-                Min = Math.min(Min, tmp.cnt);
+            if (tmp.i == maps.length - 1 && tmp.j == maps[0].length -1) {
+                answer = Math.min(answer, tmp.cnt + 1);
                 return;
             }
             
             for (int i = 0; i < 4; i++) {
-                int nx = dx[i] + tmp.x;
-                int ny = dy[i] + tmp.y;
+                int nx = tmp.i + dx[i];
+                int ny = tmp.j + dy[i];
                 
                 if (nx >= 0 && nx < maps.length && ny >= 0 && ny < maps[0].length) {
-                    if (visit[nx][ny] == false && maps[nx][ny] == 1) {
-                        visit[nx][ny] = true;
+                    if (maps[nx][ny] == 1 && visit[nx][ny] == false) {
                         q.add(new Node(nx, ny, tmp.cnt+1));
+                        visit[nx][ny] = true;
                     }
                 }
             }
         }
     }
     
-    class Node {
-        int x, y, cnt;
+    static class Node {
+        int i, j, cnt;
         
-        public Node(int x, int y, int cnt) {
-            this.x = x;
-            this.y = y;
+        public Node(int i, int j, int cnt) {
+            this.i = i;
+            this.j = j;
             this.cnt = cnt;
         }
     }
